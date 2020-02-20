4edce183c125daa70ff2cdde3e760a7727677730
Caroline Finnerty <s529428@nwmissouri.edu>
2020-02-20 09:58:12 -0600

    Initial Commit

---
+++ Beefing Bearcats/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Beefing Bearcats
+//
+//  Created by Finnerty,Caroline A on 2/20/20.
+//  Copyright © 2020 Finnerty,Caroline A. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

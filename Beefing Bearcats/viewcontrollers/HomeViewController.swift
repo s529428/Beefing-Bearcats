3dbc9e8355497902ec2cc7d298386d6029ddabc9
Caroline Finnerty <s529428@nwmissouri.edu>
2020-02-23 21:45:18 -0600

    Add the view controllers

---
+++ Beefing Bearcats/viewcontrollers/HomeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HomeViewController.swift
+//  Beefing Bearcats
+//
+//  Created by Caroline Finnerty on 2/23/20.
+//  Copyright © 2020 Finnerty,Caroline A. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBarItem.image = UIImage(named: "icons8-ios-filled-36.png")
+
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

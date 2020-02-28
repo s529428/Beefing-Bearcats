201c6305f2223e458366a60ce58fc4ef561041e9
Gangadhar <54452212+gangadhary574@users.noreply.github.com>
2020-02-27 18:40:00 -0600

    added exercise button fixed code added navigation controller and right bar button

--- Beefing Bearcats/viewcontrollers/HomeViewController.swift
+++ Beefing Bearcats/viewcontrollers/HomeViewController.swift
@@ -13,14 +13,14 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var front:UIImageView!
     @IBAction func clickMefront(sender:UIButton)
     {
-        if let path1 = Bundle.main.path(forResource: "Front", ofType: ".png")
+        if let path1 = Bundle.main.path(forResource: "Front11", ofType: ".png")
         {
             front.image=UIImage(contentsOfFile: path1)
         }
     }
     
     @IBAction func clickMeback(sender:UIButton){
-        if let path2 = Bundle.main.path(forResource: "Back", ofType: ".png")
+        if let path2 = Bundle.main.path(forResource: "Back11", ofType: ".png")
         {
             front.image=UIImage(contentsOfFile: path2)
         }
@@ -35,13 +35,24 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarItem.image = UIImage(named: "icons8-ios-filled-36.png")
-        if let path1 = Bundle.main.path(forResource: "Front", ofType: ".png")
+        navigationItem.title = "BEEFING BEARCATS"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "More info", style: .plain, target: self, action: #selector(moreInfo))
+        
+        
+        if let path1 = Bundle.main.path(forResource: "Front11", ofType: ".png")
         {
             front.image=UIImage(contentsOfFile: path1)
         }
         
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func moreInfo(){
+        let ac = UIAlertController(title: "More info", message: "This app will help users have more viared and accurate to their goals weight routines. As far as we have researched there are no free apps on the app store that helps users pick exercises based on specific muscle groups they wish to develop. This app is meant for people who do not wish to invest in a personal trainer or do not know where to start, but still want to get ‘beefy’", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel)
+        ac.addAction(action)
+        self.present(ac, animated: true)
     }
     
 
